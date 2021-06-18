dfdb6691b0bd9e73e37ee361e1675399d36c251b
林嘉軒 <mike200177@hotmail.com>
2021-06-18 23:51:01 +0800

    加入專案檔案。

---
+++ uDebug Helper/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace uDebug_Helper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_compare = new System.Windows.Forms.Button();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.childFormPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(70)))), ((int)(((byte)(77)))));
+            this.panel1.Controls.Add(this.btn_compare);
+            this.panel1.Controls.Add(this.btn_select);
+            this.panel1.Controls.Add(this.btn_load);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(932, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_compare
+            // 
+            this.btn_compare.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_compare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(70)))), ((int)(((byte)(77)))));
+            this.btn_compare.FlatAppearance.BorderSize = 0;
+            this.btn_compare.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.btn_compare.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.btn_compare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_compare.ForeColor = System.Drawing.Color.White;
+            this.btn_compare.Location = new System.Drawing.Point(525, 0);
+            this.btn_compare.Name = "btn_compare";
+            this.btn_compare.Size = new System.Drawing.Size(100, 60);
+            this.btn_compare.TabIndex = 2;
+            this.btn_compare.Text = "Compare";
+            this.btn_compare.UseVisualStyleBackColor = false;
+            this.btn_compare.Click += new System.EventHandler(this.btn_compare_Click);
+            // 
+            // btn_select
+            // 
+            this.btn_select.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_select.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(70)))), ((int)(((byte)(77)))));
+            this.btn_select.FlatAppearance.BorderSize = 0;
+            this.btn_select.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.btn_select.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.btn_select.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_select.ForeColor = System.Drawing.Color.White;
+            this.btn_select.Location = new System.Drawing.Point(425, 0);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(100, 60);
+            this.btn_select.TabIndex = 1;
+            this.btn_select.Text = "Select";
+            this.btn_select.UseVisualStyleBackColor = false;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_load.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(70)))), ((int)(((byte)(77)))));
+            this.btn_load.FlatAppearance.BorderSize = 0;
+            this.btn_load.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.btn_load.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.btn_load.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_load.ForeColor = System.Drawing.Color.White;
+            this.btn_load.Location = new System.Drawing.Point(325, 0);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(100, 60);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = false;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // childFormPanel
+            // 
+            this.childFormPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.childFormPanel.Location = new System.Drawing.Point(0, 60);
+            this.childFormPanel.Name = "childFormPanel";
+            this.childFormPanel.Size = new System.Drawing.Size(932, 493);
+            this.childFormPanel.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(932, 553);
+            this.Controls.Add(this.childFormPanel);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "MainForm";
+            this.Text = "uDebug Helper";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_compare;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Panel childFormPanel;
+    }
+}
+
---
+++ uDebug Helper/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace uDebug_Helper
+{
+    public partial class MainForm : Form
+    {
+        private Form activeForm;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //var inputs = new string[3];
+            //var client = new Client();
+            //var problem = client.GetProblem(Judge.UVa, 100);
+            //inputs[0] = problem.GetInput(problem.Inputs[9]).Replace("\n", Environment.NewLine);
+        }
+
+        private void OpenChildForm(Form childForm, object buttonSender)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            childFormPanel.Controls.Add(childForm);
+            childFormPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.LoadForm(), sender);
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.SelectForm(), sender);
+        }
+
+        private void btn_compare_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.CompareForm(), sender);
+        }
+    }
+}
